331e8d8988c7a0b19665a16e5c945778898fb4e8
Alejandro <alexdavila39@gmail.com>
2019-04-23 16:02:23 -0400

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,26 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import "../src/style.css";
+import Navbar from "./components/Navbar"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Google</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      <Navbar/>
+      <Router>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/Profile" component={Profile} />
+              <Route path="/HotSpots" component={HotSpots} />
+              <Route path="/Recent" component={Recent} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Router>
       </div>
     );
   }
 }
 
 export default App;
+
+
