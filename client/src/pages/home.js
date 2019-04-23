5795c9dcf9318015a5180268b9b8b81d2c60c57c
Alejandro <alexdavila39@gmail.com>
2019-04-23 17:21:59 -0400

    NavBar

---
+++ client/src/pages/home.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+// import Navbar from "../components/Navbar";
+
+
+class home extends Component {
+    render() {
+        return (
+            <div>
+           
+            </div>
+        );
+    }
+}
+
+export default home;
---
+++ client/src/pages/login.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    render() {
+        return (
+            <div>
+       {/* <form>
+    <input type="text" name="name" placeholder="Username" />
+    <input type="passwprd" name="password" placeholder="Username" />
+     <input type="submit" value="Submit" />
+</form> */}
+            </div>
+        );
+    }
+}
+
+export default Login;
