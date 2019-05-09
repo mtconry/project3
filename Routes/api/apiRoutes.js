82b992d817b4b589ad6fb3fc44618e4d2885717b
Alejandro <alexdavila39@gmail.com>
2019-05-09 19:25:55 -0400

    jorge edits

--- Routes/api/apiRoutes.js
+++ Routes/api/apiRoutes.js
@@ -1,68 +1,21 @@
-const User = require("../../models/users")
 const router = require("express").Router();
-const cloudinary = require('cloudinary');
-const Datauri = require('datauri');
-var multer  = require('multer');
-const path = require('path');
-
-
-//Image upload middleware and buffer converter
-var storage = multer.memoryStorage();
-var multerUpload = multer(storage).single("image");
-var dUri = new Datauri();
-const dataUri = req => dUri.format(path.extname(req.file.originalname).toString(), req.file.buffer);
-
-router.get("/test",(req, res)=>{
+const userController = require("../../controllers/userController");
+const formidable = require("formidable");
+router.route("/test")
+.get((req, res)=>{
     res.send("test1")
 });
 
-router.post("/profile", multerUpload, (req, res) =>{
-    console.log(req)
-    if(req.file){
-    console.log("hello")
-    console.log(req.file)
-    var file = dataUri(req).content;
-    let {userName} = req.body;
-    cloudinary.uploader.upload(file, (result) => {
-        console.log("hi")
-        console.log(result)
-        Users
-            .create({
-                userName: userName,
-                picture: result.secure._url,
-                email: email,
-                location: location,
-                description: description,
-                facebookId: facebookId,
-                googleId: googleId
-            }).then(function (newPic){
-                return userName.findOneAndUpdate({_id:req.user._id},{$push:{dbUsers:newPic._id}});
-            }).then(dbUsers => res.json(dbUsers))
-              .catch(err => res.status(422).json(err))
-        
+router.route("/upload")
+.post((req, res)=>{
+    const form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files) {
+        console.log(files);
     });
-}else{
-    res.status(404).json({msg: "No File"})
-}
 });
-router.post("/profilePic", multerUpload, (req, res) => {
-    if (req.files) {
-        var file = dataUri(req).content;
-        cloudinary.uploader.upload(file, (result) => {
-            User.findOneAndUpdate({
-                 _id: req.user._id 
-                },{
-                picture: result.secure_url,
-                })
-                .then(dbModel => res.json(dbModel))
-                .catch(err => res.status(422).json(err));
-            });
-    }else{
-        res.status(404).json({msg: "No File"})
-    }
-    });
 
+router.route("/user")
+    .get(userController.findAll)
+    .post(userController.create);
 
-
-module.exports = router
-
+module.exports = router;
--- Routes/api/user.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const userController = require("../../controllers/userController");
-
-router.route("/user")
-    .get(userController.findAll)
-    .post(userController.create);
-
-module.exports = router;
