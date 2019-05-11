cd351dadb5a1f1813e0abec3ddb752e3894ad751
Alejandro <alexdavila39@gmail.com>
2019-05-11 11:14:29 -0400

    update

--- client/src/pages/hotspots.js
+++ client/src/pages/hotspots.js
@@ -18,16 +18,20 @@ class HotSpots extends Component {
 
     render() {
         return (
-            <div className="container">
-            <div className ="row">
+            <div className="container" style={{textAlign:"center"}}>
+            <div className ="row" style={{textAlign:"center"}}>
             { this.state.users.map((user,i )=> (
       
-      <div key={i} className="card">
-                <img className="card-img-top" src={user.url} style={{textAlign:"center", maxHeight:"400px" ,width:"400px"}}  alt="Card image cap"/>
+      <div key={i} className="card" >
+                <img className="user-img" src={user.imageUrl} style={{textAlign:"center", maxHeight:"100px" ,width:"100px",paddingLeft:"10px", paddingTop:"10px", borderRadius:"75%"}} alt="userUpload"/>
+                <h3 className="card-title" style={{textAlign:"left", padding:"10px"}}>{user.name}</h3>
+                <div>
+                <img className="card-img-top" src={user.url} style={{ maxHeight:"600px" ,width:"600px"}}  alt="Card image cap"/>
                 <div className="card-body">
-                <h5 className="card-title">{user.name}</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <img className="user-img" src={user.imageUrl}  alt="userUpload"/>
+                
+                
+                <p className="card-text" style={{textAlign:"center",paddingBottom:"15px" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et sapien vitae ipsum ornare rhoncus. Nam rhoncus augue vitae urna.</p>
+                </div>
             </div>
         </div>
        
